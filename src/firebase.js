8067c6791d2162a37d36cee0af2a0a9b40fe9e61
Prathik Reddy <tvkp333555@gmail.com>
2021-12-28 22:23:28 +0530

    basic firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1SibLyMLiS-2cp_YzWWX0Ypabx7zYiOs",
+  authDomain: "prathik-firebase1.firebaseapp.com",
+  projectId: "prathik-firebase1",
+  storageBucket: "prathik-firebase1.appspot.com",
+  messagingSenderId: "158349976399",
+  appId: "1:158349976399:web:ce4fd9012ed63874f3db3a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
